bef100e7f9adfac58c07f8f831b7377698c60256
zackbakkal <zackbakkal@gmail.com>
2021-02-28 11:31:55 -0700

    Eureka discovery server added

--- src/main/java/com/zack/peojects/chatapp/userservice/controller/UserController.java
+++ src/main/java/com/zack/peojects/chatapp/userservice/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -32,22 +32,25 @@ public class UserController {
         return userService.getUser(username);
     }
 
-    @PutMapping("/{username}/online")
+    @PutMapping("/status/{username}/online")
     public boolean setUserOnline(@PathVariable String username)
             throws UserNameNotFoundException {
         return userService.setUserOnline(username);
     }
 
-    @PutMapping("/{username}/offline")
-    public boolean setUserOffline(@PathVariable String username)
-            throws UserNameNotFoundException {
+    @PutMapping("/status/{username}/offline")
+    public boolean setUserOffline(@PathVariable String username) {
         return userService.setUserOffline(username);
     }
 
-    @PutMapping("/{username}/{availability}")
-    public boolean updateUserAvailability(@PathVariable String username, @PathVariable String availability)
-            throws UserNameNotFoundException {
+    @PutMapping("/availability/{username}/{availability}")
+    public boolean updateUserAvailability(@PathVariable String username, @PathVariable String availability) {
         return userService.setUserAvailability(username, availability);
+    }
+
+    @GetMapping("/registered/{username}")
+    public boolean userIsRegistered(@PathVariable String username) {
+        return userService.userIsRegistered(username);
     }
 
 }
